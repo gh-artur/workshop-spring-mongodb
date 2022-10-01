e74e9871718821bfb0daa7b9c29a9a79c62079cd
Artur Santos <arturfrancisco86@gmail.com>
2022-10-01 14:03:11 -0300

    Consulta simples com @Query

--- src/main/java/com/ghartur/workshopmongo/services/PostService.java
+++ src/main/java/com/ghartur/workshopmongo/services/PostService.java
@@ -22,7 +22,9 @@ public class PostService{
 	}
 	
 	public List<Post> findByTitle(String text){
-		return repo.findByTitleContainingIgnoreCase(text);
+		return repo.searchTitle(text);
 	}
+
+	
 	
 }
